a0f35808dcdc24518f46e3af8260effd3a9932ed
Hernani Fernandes <hernanif1@gmail.com>
2021-12-26 17:16:57 +0100

    add logs and libs in typescript

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,24 @@
-import express from "express";
+import express, {
+  json,
+  NextFunction,
+  Request,
+  Response,
+  urlencoded,
+} from "express";
 import product from "./src/product";
+import logger from "morgan";
+
 const app = express();
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(logger("dev"));
+app.use((_req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 const PORT = process.env.PORT || 5050;
+
 app.use("/v1", product);
+
 app.listen(PORT, () => console.log("server running"));
